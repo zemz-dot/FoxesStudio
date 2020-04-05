271678a4424308b6f1e4792bc6db82303c1caa2b
ElloumiNATTY <61619978+foxes-studio@users.noreply.github.com>
2020-04-05 22:59:22 +0200

    Create DEF.h

---
+++ Elloumi/DEF.h
@@ -0,0 +1,8 @@
+    // Constante définissant le seuil entre les tiles traversables (blank) et les tiles solides
+    #define BLANK_TILE 5
+
+   //Constantes définissant la gravité et la vitesse max de chute
+   #define GRAVITY_SPEED 0.6
+   #define MAX_FALL_SPEED 10
+   #define JUMP_HEIGHT 13
+ 
