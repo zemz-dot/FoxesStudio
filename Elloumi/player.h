062eca162fc6cd2c836d571107762f86c005177e
ElloumiNATTY <61619978+foxes-studio@users.noreply.github.com>
2020-04-21 00:30:40 +0200

    Update player.h

--- Elloumi/player.h
+++ Elloumi/player.h
@@ -1,1 +1,12 @@
 
+ #include "structs.h"
+
+extern Gestion jeu;
+extern Hero player;
+extern Input input;
+extern Map map;
+
+/* Prototypes des fonctions utilisées */
+extern SDL_Surface *loadImage(char *name);
+extern void centerScrollingOnPlayer(void);
+extern void mapCollision(Hero *entity);
