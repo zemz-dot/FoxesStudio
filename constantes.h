4907d94e9fca996dc8be0855a475dad121d7637b
faroukyh <61096103+faroukyh@users.noreply.github.com>
2020-04-20 19:21:32 +0100

    farouk
    affichage du personnage

---
+++ constantes.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTES_H_INCLUDED
+#define CONSTANTES_H_INCLUDED
+
+
+#define TAILLE_BLOC 34
+
+enum{HAUT,BAS,GAUCHE,DROITE};
+//pour appeller les images ,qui vont etre soit link haut,soit link bas ...
+enum{VIDE,MUR,LINK};
+// le vide c la ou la personnage peut marcher
+//les murs c la ou la personnage ne peut pas aller
+//le link > c'est la personnage du jeu
+
+
+#endif // CONSTANTES_H_INCLUDED
