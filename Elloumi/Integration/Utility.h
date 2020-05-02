b5a52790f8b3a459eb7b4d0c204b0820ef552839
ElloumiNATTY <61619978+foxes-studio@users.noreply.github.com>
2020-05-02 09:49:23 -0700

    Create Utility.h

---
+++ Elloumi/Integration/Utility.h
@@ -0,0 +1,17 @@
+#ifndef FONCTIONS_H_
+#define FONCTIONS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "depane.h" 
+
+
+int collisionbb ( SDL_Rect posj , SDL_Rect posobj ) ; 
+int collisiontrigoinscrit ( SDL_Rect posj , SDL_Rect posobj ) ; 
+int CollisionTrigoCir (  SDL_Rect posj , SDL_Rect posobj );
+
+
+
+#endif /* FONCTIONS_H_ */
