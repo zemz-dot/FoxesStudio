7b64be758f676678262f92aba7e9d2b7ae6164b4
ElloumiNATTY <61619978+foxes-studio@users.noreply.github.com>
2020-06-01 00:38:24 +0200

    Create Scrolling.h

---
+++ Elloumi/Integration/Scrolling.h
@@ -0,0 +1,7 @@
+#ifndef JEU_H_INCLUDED
+#define JEU_H_INCLUDED
+#include <SDL/SDL.h>
+
+
+SDL_Rect scroll1(SDL_Surface *ecran,SDL_Surface *imageDeFond ,int continuer ,SDL_Rect positionFond) ; 
+#endif // JEU_H_INCLUDED
