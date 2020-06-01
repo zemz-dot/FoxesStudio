96f565eaf448ba46d9d759631da29a9bf5a77d26
ElloumiNATTY <61619978+foxes-studio@users.noreply.github.com>
2020-06-01 06:02:28 -0700

    Update enigf.c

--- Elloumi/Integration/enigf.c
+++ Elloumi/Integration/enigf.c
@@ -4,12 +4,12 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_mixer.h>
 #include <SDL/SDL_image.h>
-#include "game.h"
+#include "enig.h"
 
 void init_enigme(enigme * e)
 {
-	e->p.x=50;
-	e->p.y=50;	
+	e->p.x=0;
+	e->p.y=0;	
 	e->img=NULL;
 
 
@@ -21,27 +21,26 @@ void init_enigme(enigme * e)
 do{
  *alea = 1+ rand()%3;
 }while(*alea==test) ;
- sprintf(image ,"Enigme/%d.jpg",*alea);
+ sprintf(image ,"%d.jpg",*alea); // *str,const,var transforme la sortie formatee en une chaine pointee par str
 e->img = IMG_Load(image);
  SDL_BlitSurface(e->img,NULL,screen,&(e->p)) ;
   SDL_Flip(screen) ;
-
 }
  
  int solution_e (char image [])
  {
  	int solution =0 ;
  	
- 	if(strcmp(image,"Enigme/1.jpg")==0)
+ 	if(strcmp(image,"1.jpg")==0)
  	{
-     solution =1 ;
+     solution =2 ;
  	}
- 	else  	if(strcmp(image,"Enigme/2.jpg")==0)
+ 	else  	if(strcmp(image,"2.jpg")==0)
  	{
- 		solution =2;
+ 		solution =1;
 
  	}
- 	else 	if(strcmp(image,"Enigme/3.jpg")==0)
+ 	else 	if(strcmp(image,"3.jpg")==0)
  	{
  		solution =3;	
  	}
@@ -52,12 +51,12 @@ int resolution (int * running,int *run )
 {
 	SDL_Event event ;
   int r=0 ;
-
 	SDL_PollEvent(&event);
 	switch(event.type)
 	{
 		  case SDL_QUIT:
-			        *running= 1;
+			        *running= 0 ;
+                *run = 0;
 				break ;
 
        case SDL_KEYDOWN : 
@@ -80,20 +79,20 @@ int resolution (int * running,int *run )
   return r ;
 }
  
- int afficher_resultat (SDL_Surface * screen,int s,int r,enigme *en )
+ void afficher_resultat (SDL_Surface * screen,int s,int r,enigme *en)
  {
-  int x ; 
+ 
  	if (r==s)
  	{
-
-         x=1 ; 
+ 		en->img=IMG_Load("00.jpg");
+ 		SDL_BlitSurface(en->img, NULL, screen, &(en->p)) ;
+        SDL_Flip(screen);
  	}
  	else
  	{
- 
-        x=0 ; 
-        
+ 		en->img=IMG_Load("11.jpg");
+ 		SDL_BlitSurface(en->img, NULL, screen, &(en->p)) ;
+        SDL_Flip(screen);
  	}
-return x ; 
  }
  
