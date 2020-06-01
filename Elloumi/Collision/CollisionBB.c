c9f915f0305988407c7fac4015c14a18895d3165
ElloumiNATTY <61619978+foxes-studio@users.noreply.github.com>
2020-06-02 00:00:17 +0200

    Update CollisionBB.c

--- Elloumi/Collision/CollisionBB.c
+++ Elloumi/Collision/CollisionBB.c
@@ -1,17 +1,44 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
-#include "Collision.h"
+#include <SDL/SDL_mixer.h>
+#include "collison.h"
 
-int collisionbb( SDL_Rect posj , SDL_Rect posobj )
+
+
+
+SDL_Color GetPixel (SDL_Surface* pSurface, int x, int y)
 {
- int x ; 
-if ( ((posj.y+posj.h>=posobj.y)&&(posj.y<=posobj.y+posobj.h)&&(posj.x+posj.w>=posobj.x)&&(posj.x<=posobj.x+posobj.w)) )
-{
-	x=1 ; 
+    SDL_Color color;
+    Uint32 col = 0 ;
+
+    //determine position
+    char* pPosition = ( char* ) pSurface->pixels ;
+
+    //offset by y
+    pPosition += ( pSurface->pitch * y ) ;
+
+    //offset by x
+    pPosition += ( pSurface->format->BytesPerPixel * x ) ;
+
+    //copy pixel data
+    memcpy ( &col, pPosition, pSurface->format->BytesPerPixel ) ;
+
+    //convert color
+    SDL_GetRGB ( col, pSurface->format, &color.r, &color.g, &color.b ) ;
+    return ( color ) ;
 }
 
-else x=0 ; 
-return x ;
+int collision(SDL_Surface * BackGround_Menu, SDL_Rect *position_perso, SDL_Surface *perso)
+{
+int coll=0;
+
+    SDL_Color col1 = GetPixel( BackGround_Menu, position_perso->x + perso->w-30, position_perso->y );
+    if(col1.r ==0 && col1.g == 0 && col1.b == 0)
+    {
+        coll=1;
+    }
+    
+return coll;
 }
