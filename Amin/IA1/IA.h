bf3170760342ffbf39f8a298589ea2c02ba051fb
aminhl <62307781+aminhl@users.noreply.github.com>
2020-06-09 20:59:55 +0200

    Add files via upload

---
+++ Amin/IA1/IA.h
@@ -0,0 +1,31 @@
+#ifndef IA_H_INCLUDED
+#define IA_H_INCLUDED
+
+
+typedef struct
+{
+  int x ;
+  int y ;
+  float rayon ;
+
+}Circle;
+
+typedef struct
+{
+
+  SDL_Surface *img ;
+  SDL_Rect pos ;
+  Circle c ;
+  SDL_Rect pos_text ;
+
+}Objet ;
+
+
+void intialiser(Objet *perso ,Objet *zombie) ;
+void setup (SDL_Surface *screen,Objet *objet1,Objet *objet2) ;
+float calculer_distance (Objet *objet1,Objet *objet2) ;
+int verif_collision ( Objet *objet1,Objet *objet2,float D1 ) ;
+void deplacement_objet(Objet *objet,int *running) ;
+
+
+#endif // IA_H_INCLUDED
