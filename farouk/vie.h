12cce5f1a0c4a7370a87e0df7f4c2f0d179be39c
faroukyh <61096103+faroukyh@users.noreply.github.com>
2020-06-14 15:34:59 +0100

    Add files via upload

---
+++ farouk/vie.h
@@ -0,0 +1,18 @@
+#ifndef VIE_H_
+#define VIE_H_
+typedef struct vie
+{
+	
+	SDL_Rect position;
+	SDL_Surface *fond1;
+	SDL_Surface *fond2;
+	SDL_Surface *fond3;
+	SDL_Surface *fond4;
+	SDL_Surface *fond5;
+        SDL_Surface *fond6;
+}vie;
+
+void initialiservie(vie *vie);
+void affichervie(vie *vie,SDL_Rect *posj , SDL_Rect posobj,SDL_Surface *ecran , int *i);
+int collisionbb( SDL_Rect posj , SDL_Rect posobj );
+#endif
